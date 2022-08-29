3ee7f5124ed3ef966956d99eec058fc94e739a45
Magnesium- <yyong007@outlook.com>
2022-08-29 23:43:45 +0800

    feat(init): init project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gn
+
 generator a node project
+
+## 支持
+
+- esm/cjs
+- typescript
---
+++ src/cli.js
@@ -0,0 +1,17 @@
+#! /usr/bin/node
+import { Command } from 'commander'
+
+import start from './start.js'
+
+const program = new Command()
+
+program
+  .description('create a new project')
+  .option('-n, --name <name>', 'name of the new project')
+  .option('-t, --typescript', 'typescript support', true)
+  .option('-g, --git', 'git support', true)
+  .action(async function (options) {
+    await start(options)
+  })
+
+program.parse(process.argv)
