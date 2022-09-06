69ea70a6c36ceb462adf1f0bffb38f2e9b69fcac
Magnesium- <yyong007@outlook.com>
2022-09-06 22:04:57 +0800

    feat(rollup install): rollup install

--- src/core/rollup.config.js
+++ src/core/rollup.config.js
@@ -1,8 +1,18 @@
 import logger from '../utils/logger.js'
 import { genConfigFileByName } from '../utils/configFile.js'
+import { getPkgJson, setPkgJson } from '../utils/package.js'
+import { execRun } from '../utils/index.js'
+import { getConfig } from '../utils/config.js'
 
 export default async function initRollupConfig() {
-  await genConfigFileByName('rollup.config.js')
+  const pkgJson = await getPkgJson()
 
+  pkgJson['scripts']['compile'] = 'rollup -c'
+
+  await genConfigFileByName('rollup.config.js')
+  await setPkgJson(pkgJson)
+
+  const { npmManager } = getConfig()
+  await execRun(npmManager === 'yarn' ? 'yarn' : `${npmManager} install`)
   logger.info('✅ rollup 配置文件初始化成功')
 }
